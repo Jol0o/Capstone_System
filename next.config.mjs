64a8097871c489fbd8b68c22e50aade43d859933
Jol0o <jloyd9836@gmail.com>
2024-10-15 15:26:14 +0800

    Refactor package.json and next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['firebasestorage.googleapis.com', 'res.cloudinary.com'],
-    },
+  images: {
+    domains: ['firebasestorage.googleapis.com', 'res.cloudinary.com'],
+  },
 };
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
+export default withPWA(nextConfig);
