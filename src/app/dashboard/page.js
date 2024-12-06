65780cd7cfeebebebf54632444b8a7ba0bdf261b
Jol0o <jloyd9836@gmail.com>
2024-12-07 01:45:21 +0800

    Remove tab state from localStorage on logout for improved state management

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -105,6 +105,7 @@ useEffect(() => {
       localStorage.removeItem("token");
       localStorage.removeItem("user");
       localStorage.removeItem("admin");
+       localStorage.removeItem("tab");
       router.push("/");
     } catch (e) {
       console.log(e);
